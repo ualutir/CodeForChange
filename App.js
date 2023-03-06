8149ddffa615a7197e74521a96e188f168f2a2f8
Iranga Aluthgedara <icaluthgedara@gmail.com>
2023-03-06 21:20:05 +0800

    Initial Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text>Hello</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FF5733',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CodeForChange
 Code for Change
+
+
